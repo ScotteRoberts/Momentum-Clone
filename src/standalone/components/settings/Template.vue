65362ea0ab3a2b0a7385e95ac675b6791c8640f2
Scott Roberts <s.e.roberts0@gmail.com>
2019-02-20 21:22:04 -0800

    Fix broken Github link

--- src/standalone/components/settings/Template.vue
+++ src/standalone/components/settings/Template.vue
@@ -2,54 +2,57 @@
   <div class="columns is-gapless">
     <div class="column is-left is-3">
       <aside class="menu">
-        <p class="menu-label">
-          Settings
-        </p>
+        <p class="menu-label">Settings</p>
         <ul class="menu-list">
-          <li><a @click="activeMenu = 'general'">General</a></li>
-          <li><a @click="activeMenu = 'todo'">Todo</a></li>
-          <li><a @click="activeMenu = 'mantra'">Mantras</a></li>
-          <li><a @click="activeMenu = 'photo'">Photos</a></li>
-          <li><a @click="activeMenu = 'quote'">Quotes</a></li>
-          <li><a @click="activeMenu = 'link'">Links</a></li>
-          <li><a @click="activeMenu = 'balance'">Balance</a></li>
-        </ul>
-        <p class="menu-label">
-          Documentation
-        </p>
-        <ul class="menu-list">
-          <li><a
-              href="https://scotteroberts.github.io"
-              target="_blank"
-            >About</a></li>
-          <li><a
-              href="https://github.com/ScotteRoberts/Personal-Projects/tree/master/Chrome%20Extensions/momentum-clone"
-              target="_blank"
-            >Github</a></li>
           <li>
-            <a
-              href="https://www.linkedin.com/in/scott-e-roberts"
-              target="_blank"
-            >LinkedIn
-            </a>
+            <a @click="activeMenu = 'general'">General</a>
+          </li>
+          <li>
+            <a @click="activeMenu = 'todo'">Todo</a>
+          </li>
+          <li>
+            <a @click="activeMenu = 'mantra'">Mantras</a>
+          </li>
+          <li>
+            <a @click="activeMenu = 'photo'">Photos</a>
+          </li>
+          <li>
+            <a @click="activeMenu = 'quote'">Quotes</a>
+          </li>
+          <li>
+            <a @click="activeMenu = 'link'">Links</a>
+          </li>
+          <li>
+            <a @click="activeMenu = 'balance'">Balance</a>
+          </li>
+        </ul>
+        <p class="menu-label">Documentation</p>
+        <ul class="menu-list">
+          <li>
+            <a href="https://scotteroberts.github.io" target="_blank">About</a>
+          </li>
+          <li>
+            <a href="https://github.com/ScotteRoberts/Momentum-Clone" target="_blank">Github</a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/in/scott-e-roberts" target="_blank">LinkedIn</a>
           </li>
         </ul>
       </aside>
     </div>
     <div class="is-divider-vertical"></div>
     <div class="column is-right">
-      <balance-settings v-show="activeMenu === 'balance'" />
-      <general-settings v-show="activeMenu === 'general'" />
-      <link-settings v-show="activeMenu === 'link'" />
-      <mantra-settings v-show="activeMenu === 'mantra'" />
-      <photo-settings v-show="activeMenu === 'photo'" />
-      <quote-settings v-show="activeMenu === 'quote'" />
-      <todo-settings v-show="activeMenu === 'todo'" />
+      <balance-settings v-show="activeMenu === 'balance'"/>
+      <general-settings v-show="activeMenu === 'general'"/>
+      <link-settings v-show="activeMenu === 'link'"/>
+      <mantra-settings v-show="activeMenu === 'mantra'"/>
+      <photo-settings v-show="activeMenu === 'photo'"/>
+      <quote-settings v-show="activeMenu === 'quote'"/>
+      <todo-settings v-show="activeMenu === 'todo'"/>
     </div>
     <!-- HACK: Added extra padding to the end to make the settings panels centered -->
     <div class="column is-1"></div>
   </div>
-
 </template>
 
 <script>
